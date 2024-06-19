16619e1f641ee0dce4418cbcd25b3fed2b369c37
Saamu02 <iusamairfan@gmail.com>
2024-06-19 09:19:20 +0500

    Project created + assets added

---
+++ Restart/Restart/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Restart
+//
+//  Created by Ussama Irfan on 19/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
